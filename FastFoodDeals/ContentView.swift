254c9029f94cb124ae8605bbc89622b53d90337c
Robert <robjackfar@gmail.com>
2024-08-19 16:41:39 -0500

    Initial Commit

---
+++ FastFoodDeals/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FastFoodDeals
+//
+//  Created by Robert Farrar on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastFoodDeals/FastFoodDealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastFoodDealsApp.swift
+//  FastFoodDeals
+//
+//  Created by Robert Farrar on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FastFoodDealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
